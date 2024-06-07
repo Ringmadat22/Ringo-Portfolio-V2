f54dbb4b5b33208d2df23e4c6097c601f37470a0
Ring Madat <smithemma329@gmail.com>
2024-06-07 13:26:20 +0300

    main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import './App.css';
+import { Particles } from "react-particles";
+import { loadFull } from "tsparticles";
 
 function App() {
+  const particlesInit = async (main) => {
+    await loadFull(main);
+  };
+
   return (
-    <>
-      <h1>Hi this is a test</h1>
-    </>
+    <div className="App">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={{
+          // customize your particles options here
+        }}
+      />
+    </div>
   );
 }
 
