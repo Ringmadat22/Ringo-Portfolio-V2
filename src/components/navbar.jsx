98d438f5e1184ce000d5ff90b9f82289a2910522
Ring Madat <smithemma329@gmail.com>
2024-06-14 18:18:11 +0300

    main

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,43 +1,37 @@
-import React from 'react'
+import React from 'react';
 
-function navbar() {
+function Navbar() {
   return (
-    <section className='border-solid flex bg-inherit justify-center items-center h-20 p-15'>
-        <div>
-            <nav className='flex w-70 bg-blue-300 mt-20 border-solid border-1 border-black justify-between rounded-3xl p-5'>
-                <div className='logo'>RINGO</div>
-                <div className='right-nav'>
-                    <ul className='flex'>
-                        <li>
-                            <a href='/'>Home</a>
-                        </li>
-
-                        <li>
-                            <a href='/'>About</a>
-                        </li>
-
-                        <li>
-                            <a href='/'>Skills</a>
-                        </li>
-
-                        <li>
-                            <a href='/'>Experiece</a>
-                        </li>
-
-                        <li>
-                            <a href='/'>Projects</a>
-                        </li>
-
-                        <li>
-                            <a href='/'>Contact</a>
-                        </li>
-
-                    </ul>
-                </div>
-            </nav>
-        </div>
+    <section className='border-solid mt-0 pt-0 flex bg-inherit w-full justify-center items-center h-20 '>
+      <div className='w-3/5  bg-inherit  rounded-lg '>
+        <nav className='flex w-full bg-blue-300 mt-20 border-solid border-1 border-black justify-between rounded-full p-5'>
+          <div className='logo'>RINGO</div>
+          <div className='right-nav'>
+            <ul className='flex'>
+              <li className='mx-6 cursor-pointer'>
+                <a href='/'>Home</a>
+              </li>
+              <li className='mx-6'>
+                <a href='/'>About</a>
+              </li>
+              <li className='mx-6'>
+                <a href='/'>Skills</a>
+              </li>
+              <li className='mx-6'>
+                <a href='/'>Experience</a>
+              </li>
+              <li className='mx-6'>
+                <a href='/'>Projects</a>
+              </li>
+              <li className='mx-6'>
+                <a href='/'>Contact</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </section>
-  )
+  );
 }
 
-export default navbar
+export default Navbar;
